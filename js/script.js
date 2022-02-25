28a1e2afa9d647cd59b56e2723397cdb8f823bbf
Zhomart Zhetpisbekov <zhetpisbekovzh@gmail.com>
2022-02-26 00:12:35 +0600

    Added Menu Items page

--- js/script.js
+++ js/script.js
@@ -17,9 +17,13 @@ document.addEventListener("DOMContentLoaded",
 var dc = {};
 
 var homeHtml = "snippets/home-snippet.html";
-var allCategoriesUrl = "https://davids-restaurant.herokuapp.com/categories.json";
+var allCategoriesUrl = "https://www.davidchuschinabistro.com/categories.json";
 var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
+
+var menuItemsUrl = "https://www.davidchuschinabistro.com/menu_items.json?category=";
+var menuItemsTitleHtml = "snippets/menu-items-title.html";
+var menuItemHtml = "snippets/menu-item.html";
 
 // Convinience method for inserting innerHTML for 'select'
 var insertHTML = function (selector, html) {
@@ -81,6 +85,14 @@ dc.loadMenuCategories = function () {
 		buildAndShowCategoriesHtml);
 };
 
+// Load Menu Items view
+dc.loadMenuItems = function (categoryShort) {
+	showLoading("#main-content");
+	$ajaxUtils.sendGetRequest(
+		menuItemsUrl + categoryShort,
+		buildAndShowMenuItemsHtml);
+};
+
 // Builds HTML for the categories page based on
 // the data from server
 function buildAndShowCategoriesHtml (categories) {
@@ -96,6 +108,27 @@ function buildAndShowCategoriesHtml (categories) {
 																		categoriesTitleHtml,
 																		categoryHtml);
 					insertHTML("#main-content", categoriesViewHtml);
+				},
+				false);
+		},
+		false);
+};
+
+// Builds HTML for the Menu Item page based on
+// the data from server
+function buildAndShowMenuItemsHtml (menuItemsObject) {
+	// Load title snippet of the Menu Page
+	$ajaxUtils.sendGetRequest(
+		menuItemsTitleHtml,
+		function (menuItemsTitleHtml) {
+			$ajaxUtils.sendGetRequest(
+				menuItemHtml,
+				function (menuItemHtml) {
+					var menuItemsViewHtml = 
+						buildMenuItemsViewHtml (menuItemsObject,
+																		menuItemsTitleHtml,
+																		menuItemHtml);
+					insertHTML("#main-content", menuItemsViewHtml);
 				},
 				false);
 		},
@@ -122,6 +155,47 @@ function buildCategoriesViewHtml (categories,
 		html = insertProperty(html, "short_name", short_name);
 		finalHtml += html;
 
+	}
+
+	finalHtml += "</section>";
+	return finalHtml;
+
+};
+
+// Builds Menu Items View Html
+// using menu items data and snippets html
+function buildMenuItemsViewHtml(menuItemsObject,
+																menuItemsTitleHtml,
+																menuItemHtml) {
+
+	menuItemsTitleHtml = insertProperty(menuItemsTitleHtml,
+																			"name",
+																			menuItemsObject.category.name);
+	menuItemsTitleHtml = insertProperty(menuItemsTitleHtml,
+																			"special_instructions",
+																			menuItemsObject.category.special_instructions);
+
+	var finalHtml = menuItemsTitleHtml;
+	finalHtml += "<section class='row'>";
+
+	var catShortName = menuItemsObject.category.short_name;
+	var menuItems = menuItemsObject.menu_items;
+
+	// Loop over menu items 
+	for (var i = 0; i < menuItems.length; i++) {
+		var html = menuItemHtml;
+		var short_name = menuItems[i].short_name;
+		var name = "" + menuItems[i].name;
+		var price_large = menuItems[i].price_large;
+		var description = menuItems[i].description;
+
+		html = insertProperty(html, "catShortName", catShortName);
+		html = insertProperty(html, "short_name", short_name);
+		html = insertProperty(html, "name", name);
+		html = insertProperty(html, "price_large", price_large);
+		html = insertProperty(html, "description", description);
+
+		finalHtml += html;
 	}
 
 	finalHtml += "</section>";
